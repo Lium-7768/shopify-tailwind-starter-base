d16453147f78c6895b690fe1b3215ee742286842
leonli-ai <leonli@aaxis.io>
2024-12-23 19:48:43 +0800

    add storefront api

---
+++ assets/store-front-api.js
@@ -0,0 +1,30 @@
+const client = ShopifyStorefrontAPIClient.createStorefrontApiClient({
+  storeDomain: Shopify.shop,
+  apiVersion: '2024-10',
+  publicAccessToken: '5d2a18b43a2a6006608cd6ffcd6dc28e',
+});
+
+
+// getProductBySkuId
+
+const getProductByIdStr = `
+  query getProductById($id: ID!) {
+    product(id: $id) {
+      title
+      onlineStoreUrl
+    }
+  }
+`;
+
+async function getProductById(id) {
+  try {
+    const data = await client.request(getProductByIdStr, {
+        variables: {
+          id: id,
+        },
+      });
+    return data;
+  } catch (error) { 
+    console.error('Error getting product by id:', error.message);
+  }
+}
